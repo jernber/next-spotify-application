6ef7730dc8310faf2c1b2aa14731e6075a742585
www-notanemailaddress <129209470+www-notanemailaddress@users.noreply.github.com>
2023-08-08 11:08:47 -0500

    updated icon to use as element

--- spotify-clone/components/SidebarItem.tsx
+++ spotify-clone/components/SidebarItem.tsx
@@ -7,8 +7,8 @@ interface SidebarItemProps{
     active?: boolean;
     href: string;
 }
-
-const SidebarItem:React.FC<SidebarItemProps> = ({icon, label, active, href}) => {
+// using icon as an element
+const SidebarItem:React.FC<SidebarItemProps> = ({icon: Icon, label, active, href}) => {
 
   return (
     <Link href={href} className={twMerge("flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1", active && "text-white")}>
