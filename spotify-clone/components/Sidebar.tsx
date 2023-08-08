043d17c450db7bce8f7fc7562af6df2720476665
www-notanemailaddress <129209470+www-notanemailaddress@users.noreply.github.com>
2023-08-08 10:37:03 -0500

    Created Memoization objects

--- spotify-clone/components/Sidebar.tsx
+++ spotify-clone/components/Sidebar.tsx
@@ -1,12 +1,37 @@
 "use client";
 
+import { usePathname } from "next/navigation"
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
+
 interface SidebarProps{
     children: React.ReactNode
 }
+
+// Using children as a means to pass server components through client components
 const Sidebar: React.FC<SidebarProps> = ({children}) => {
-  return (
-    <div>{children}</div>
-  )
+    const pathname = usePathname()
+
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/',
+        },
+        {
+            icon: BiSearch,
+            label: 'Search',
+            active: pathname == '/search',
+            href: '/search'
+        }
+    ], [pathname])
+
+    return (
+        
+        <div>{children}</div>
+    )
 }
 
 export default Sidebar
