2c008c19b453245b9d14d1e1ab0fa80c54b4fb13
www-notanemailaddress <129209470+www-notanemailaddress@users.noreply.github.com>
2023-08-14 12:51:54 -0500

    Styled Home and search for mobile

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -3,6 +3,8 @@
 import { twMerge } from "tailwind-merge";
 import { useRouter } from "next/navigation";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
+import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
 
 interface HeaderProps{
     children: React.ReactNode;
@@ -27,9 +29,13 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
                     <RxCaretRight size={35} className="text-white" />
                 </button>
             </div>
+            {/* Mobile Styling */}
             <div className="flex md:hidden gap-x-2 items-center">
-                <button>
-                    <HiHome />
+                <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                    <HiHome className="text-black" size={20} />
+                </button>
+                <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                    <BiSearch className="text-black" size={20} />
                 </button>
             </div>
         </div>
