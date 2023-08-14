9b19904fa352450b86ee30db3cdc9a29e944b7ac
www-notanemailaddress <129209470+www-notanemailaddress@users.noreply.github.com>
2023-08-14 12:49:39 -0500

    Forward and Back Navigation buttons on full size devices

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -2,7 +2,7 @@
 
 import { twMerge } from "tailwind-merge";
 import { useRouter } from "next/navigation";
-import { RxCaretLeft } from "react-icons/rx"
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
 
 interface HeaderProps{
     children: React.ReactNode;
@@ -20,9 +20,16 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
     <div className={twMerge("h-fit bg-gradient-to-b from-emerald-800 p-6", className)}>
         <div className="w-full mb-4 flex items-center justify-between">
             <div className="hidden md:flex gap-x-2 items-center">
+                <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition" onClick={() => router.back()}>
+                    <RxCaretLeft size={35} className="text-white" />
+                </button>
+                <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition" onClick={() => router.forward()}>
+                    <RxCaretRight size={35} className="text-white" />
+                </button>
+            </div>
+            <div className="flex md:hidden gap-x-2 items-center">
                 <button>
-                    <RxCaretLeft />
-
+                    <HiHome />
                 </button>
             </div>
         </div>
