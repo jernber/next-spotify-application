66b4f3ea093af4456913602ebba8005a1406f433
www-notanemailaddress <129209470+www-notanemailaddress@users.noreply.github.com>
2023-08-14 12:46:10 -0500

    Header styling

--- spotify-clone/components/Header.tsx
+++ spotify-clone/components/Header.tsx
@@ -1,6 +1,32 @@
-const Header = () => {
+"use client";
+
+import { twMerge } from "tailwind-merge";
+import { useRouter } from "next/navigation";
+import { RxCaretLeft } from "react-icons/rx"
+
+interface HeaderProps{
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+    const router = useRouter();
+
+    const handleLogout = () => {
+        //handle logout
+    }
+
   return (
-    <div>Hello Header</div>
+    <div className={twMerge("h-fit bg-gradient-to-b from-emerald-800 p-6", className)}>
+        <div className="w-full mb-4 flex items-center justify-between">
+            <div className="hidden md:flex gap-x-2 items-center">
+                <button>
+                    <RxCaretLeft />
+
+                </button>
+            </div>
+        </div>
+    </div>
   )
 }
 
