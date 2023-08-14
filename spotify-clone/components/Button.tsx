8ac60a219e1694404a2825bcea0c07eaa88c2909
www-notanemailaddress <129209470+www-notanemailaddress@users.noreply.github.com>
2023-08-14 13:00:39 -0500

    finished button component

--- spotify-clone/components/Button.tsx
+++ spotify-clone/components/Button.tsx
@@ -14,7 +14,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
 }, ref) => {
     return (
         <button type={type}
-        className={twMerge("w-full rounded-full ")}>
+        className={twMerge("w-full rounded-full bg-green-500 border border-transparent px-3 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-black font-bold hover:opacity-75 transition", className)} disabled={disabled} ref={ref}{...props}>
             {children}
         </button>
     )
