a2871431ec6a405bac717dc690f0dda835ad20f9
www-notanemailaddress <129209470+www-notanemailaddress@users.noreply.github.com>
2023-08-14 13:46:12 -0500

    Styled play button on liked songs

--- spotify-clone/components/ListItem.tsx
+++ spotify-clone/components/ListItem.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation"
 import Image from "next/image"
-
+import { FaPlay } from "react-icons/fa"
 interface ListItemProps{
     image: string;
     name: string;
@@ -23,7 +23,12 @@ const ListItem: React.FC<ListItemProps> = ({
         <div className="relative min-h-[64px] min-w-[64px]">
             <Image className="object-cover"  fill src={image} alt="Image"  />
         </div>
-        <p>{name}</p>
+        <p className="font-medium truncate py-5">
+            {name}
+        </p>
+        <div className="absolute transition opacity-0 rounded-full flex items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+            <FaPlay className="text-black" />
+        </div>
     </button>
   )
 }
