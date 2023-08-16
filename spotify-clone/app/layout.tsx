b7a3055aac80034e68756be5581455d985a338aa
jernber <oraclemars@gmail.com>
2023-08-16 12:39:19 -0500

    Added UserProvider to main App

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
 import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,9 +20,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <SupabaseProvider>
-          <Sidebar>
+          <UserProvider>
+            <Sidebar>
             {children}
           </Sidebar>
+          </UserProvider>
+          
         </SupabaseProvider>
          
         
