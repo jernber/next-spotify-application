6c8c3efebbee58474cda132c33766994b97ce6d3
jernber <oraclemars@gmail.com>
2023-08-22 14:52:52 -0500

    Added toaster provider and updated library to trigger authModal

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -1,10 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+
 import Sidebar from '@/components/Sidebar'
 import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -20,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
