13a1ddaa6d10e47a616a14baa078a7d51cfa677e
jernber <oraclemars@gmail.com>
2023-08-22 13:22:11 -0500

    Created AuthModal

---
+++ spotify-clone/components/AuthModal.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import Modal from "./Modal"
+
+const AuthModal = () => {
+  return (
+    <Modal title="Welcome Back" description="Log into your account" isOpen onChange={() => {}}>
+        Auth Modal Children!
+    </Modal>>
+  )
+}
+
+export default AuthModal
