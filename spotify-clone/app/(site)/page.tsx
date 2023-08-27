f0695ad7f4fc2012885fa5df1a04fd01e45445a3
jernber <oraclemars@gmail.com>
2023-08-27 11:43:47 -0500

    moved page content and added additional styling

--- spotify-clone/app/(site)/page.tsx
+++ spotify-clone/app/(site)/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
         </div>
-        <PageContent />
+        <PageContent songs={songs} />
       </div>
     </div>
   )
