6744b671f12841d9a43d5ac31f95898844080148
jernber <oraclemars@gmail.com>
2023-08-27 12:01:13 -0500

    added tiles for images , and allowed images on next.config

--- spotify-clone/components/SongItem.tsx
+++ spotify-clone/components/SongItem.tsx
@@ -2,6 +2,7 @@
 
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types"
+import Image from "next/image";
 
 interface SongItemProps {
     data: Song;
@@ -12,7 +13,11 @@ const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
     const imagePath = useLoadImage(data)
 
     return (
-        <div onClick={() => onClick(data.id)} className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3">SongItem</div>
+        <div onClick={() => onClick(data.id)} className="relative group flex flex-col items-center justify-center rounded-md overflow-hidden gap-x-4 bg-neutral-400/5 cursor-pointer hover:bg-neutral-400/10 transition p-3">
+           <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
+           <Image className="object-cover" src={imagePath || '/images/liked.png'} fill alt="Image" />
+           </div> 
+        </div>
     )
 }
 
