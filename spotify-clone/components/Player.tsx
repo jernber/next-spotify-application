e473353fc1baf0dfcde70444eab1728e4e09aa17
jernber <oraclemars@gmail.com>
2023-08-27 16:12:57 -0500

    Started player content

--- spotify-clone/components/Player.tsx
+++ spotify-clone/components/Player.tsx
@@ -3,6 +3,7 @@
 import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer"
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
     const player = usePlayer();
@@ -17,7 +18,8 @@ const Player = () => {
 
     return (
         <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-            Player
+            {/* Key used so that if user skips song we destroy playercontent and start fresh*/}
+            <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
         </div>
     )
 }
---
+++ spotify-clone/components/PlayerContent.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+
+const PlayerContent = () => {
+  return (
+    <div>PlayerContent</div>
+  )
+}
+
+export default PlayerContent
