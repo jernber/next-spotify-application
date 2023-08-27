f8b58693d3016b595fa7c063ae5027b4c19d818a
jernber <oraclemars@gmail.com>
2023-08-27 14:47:54 -0500

    Updated searchcontent for no returns

--- spotify-clone/app/search/components/SearchContent.tsx
+++ spotify-clone/app/search/components/SearchContent.tsx
@@ -1,10 +1,18 @@
 'use client'
 
-import React from 'react'
+import { Song } from "@/types"
 
+interface SearchContentInterface {
+    songs: Song[]
+}
 
+const SearchContent: React.FC<SearchContentInterface> = ({ songs }) => {
+    if (songs.length === 0 ){
+        return(
+            <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">No songs found.</div>
+        )
+    }
 
-const SearchContent = () => {
   return (
     <div>SearchContent</div>
   )
