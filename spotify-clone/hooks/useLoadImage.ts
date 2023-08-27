6e896ab6350c5d41e24b9ffb4586941fc5d2b793
jernber <oraclemars@gmail.com>
2023-08-27 12:43:05 -0500

    updated imports on useLoadImage

--- spotify-clone/hooks/useLoadImage.ts
+++ spotify-clone/hooks/useLoadImage.ts
@@ -1,14 +1,12 @@
 import { useSupabaseClient } from "@supabase/auth-helpers-react"
 
 import { Song } from "@/types"
-import { isNullishCoalesce } from "typescript"
 
 const useLoadImage = (song: Song) => {
     const supabaseClient = useSupabaseClient()
 
     if (!song){
         return null
-        
     }
 
     const { data: imageData } = supabaseClient.storage.from('images').getPublicUrl(song.image_path)
