55a867af6b503bb4b880248965b6cdb40b09244b
jernber <oraclemars@gmail.com>
2023-08-27 16:17:59 -0500

    Added MediaItem to player, and prepared other styles

--- spotify-clone/components/PlayerContent.tsx
+++ spotify-clone/components/PlayerContent.tsx
@@ -1,10 +1,33 @@
 'use client'
 
+import { Song } from "@/types"
+import MediaItem from "./MediaItem"
+import LikeButton from "./LikeButton"
 
-const PlayerContent = () => {
-  return (
-    <div>PlayerContent</div>
-  )
+interface PlayerContentProps {
+    song: Song
+    songUrl: string
+}
+
+const PlayerContent: React.FC<PlayerContentProps> = ({song, songUrl}) => {
+
+
+    return (
+        <div className="grid grid-cols md:grid-cols-3 h-full">
+            <div className="flex w-full justify-start">
+                <div className="flex items-center gap-x-4"> 
+                    <MediaItem data={song} />
+                    <LikeButton songId={song.id} />
+                </div>
+            </div>
+
+            <div className="flex md:hidden col-auto w-full justify-end items-center">
+                <div className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer" onClick={() => {}}>
+                    <Icon />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default PlayerContent
