44872e9c7204143b089d69929b076337fbd9b537
jernber <oraclemars@gmail.com>
2023-08-27 11:51:21 -0500

    Created SongItem used to display songs

---
+++ spotify-clone/components/SongItem.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { Song } from "@/types"
+
+interface SongItemProps {
+    data: Song;
+    onClick: (id: string) => void
+}
+
+const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
+  return (
+    <div>SongItem</div>
+  )
+}
+
+export default SongItem
