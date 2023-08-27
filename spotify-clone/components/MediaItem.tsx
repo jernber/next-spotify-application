53b6237a6e864607866d628967331571dca3a8ff
jernber <oraclemars@gmail.com>
2023-08-27 12:38:31 -0500

    started Media Item

---
+++ spotify-clone/components/MediaItem.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import useLoadImage from "@/hooks/useLoadImage";
+import { Song } from "@/types";
+
+interface MediaItemProps {
+    data: Song;
+    onClick?: (id: string) => void
+    
+}
+const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+    const imageUrl = useLoadImage(data)
+    const handleClick = () => {
+        if (onClick){
+            return onClick(data.id)
+        }
+
+        // TODO: Default turn on player
+    }
+    
+    return (
+        <div>MediaItem</div>
+    )
+}
+
+export default MediaItem
