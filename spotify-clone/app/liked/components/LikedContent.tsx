c78b9148e10a4359056a3bf4c3b3d4a39623d461
jernber <oraclemars@gmail.com>
2023-08-27 15:31:42 -0500

    Created LikedContent component

---
+++ spotify-clone/app/liked/components/LikedContent.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+interface LikedContentProps{}
+
+const LikedContent:React.FC<LikedContentProps> = () => {
+  return (
+    <div>LikedContent</div>
+  )
+}
+
+export default LikedContent
