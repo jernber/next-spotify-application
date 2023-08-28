ee1583bba04906f399e82832a0fac178ba7bc9ff
jernber <oraclemars@gmail.com>
2023-08-27 21:10:37 -0500

    wired functions to appropriate onclick events

--- spotify-clone/components/PlayerContent.tsx
+++ spotify-clone/components/PlayerContent.tsx
@@ -84,6 +84,13 @@ const PlayerContent: React.FC<PlayerContentProps> = ({song, songUrl}) => {
         }
     }
     
+    const toggleMute = () => {
+        if (volume === 0){
+            setVolume(1)
+        } else {
+            setVolume(0)
+        }
+    }
 
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -95,22 +102,22 @@ const PlayerContent: React.FC<PlayerContentProps> = ({song, songUrl}) => {
             </div>
 
             <div className="flex md:hidden col-auto w-full justify-end items-center">
-                <div onClick={onPlayPrevious} className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer" >
+                <div onClick={handlePlay} className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer" >
                     <Icon size={30} className="text-black" />
                 </div>
             </div>
 
             <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
-                <AiFillStepBackward size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" onClick={() => {}} />
-                <div onClick={onPlayNext} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
+                <AiFillStepBackward size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" onClick={onPlayPrevious} />
+                <div onClick={handlePlay} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
                     <Icon size={30} className="text-black" />
                 </div>
-                <AiFillStepForward onClick={() => {}} size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" />
+                <AiFillStepForward onClick={onPlayNext} size={30} className="text-neutral-400 cursor-pointer hover:text-white transition" />
             </div>
 
             <div className="hidden md:flex w-full justify-end pr-2">
                 <div className="flex items-center gap-x-2 w-[120px]">
-                    <VolumeIcon onClick={() => {}} className="cursor-pointer" size={34} />
+                    <VolumeIcon onClick={toggleMute} className="cursor-pointer" size={34} />
                     <Slider />
                 </div>
             </div>
