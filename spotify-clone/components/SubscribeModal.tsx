a5c6646b6456266b51a82492f4b7bc46012d3488
jernber <oraclemars@gmail.com>
2023-08-28 12:26:19 -0500

    Created Subscribe modal and changed modal styling

---
+++ spotify-clone/components/SubscribeModal.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import Modal from "./Modal"
+
+const SubscribeModal = () => {
+    let content = (
+        <div className="text-center">No Products available</div>
+    )
+    return (
+        <Modal title="Only for premium Users" description="Listen to music with Spotify Premium" isOpen onChange={() => {}}>
+            Subscription
+        </Modal>
+    )
+}
+
+export default SubscribeModal
