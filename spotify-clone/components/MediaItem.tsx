fccc64a62ba6d1523b2056e801c2fc57cc88b700
jernber <oraclemars@gmail.com>
2023-08-28 13:34:07 -0500

    set song to player

--- spotify-clone/components/MediaItem.tsx
+++ spotify-clone/components/MediaItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types";
 
 interface MediaItemProps {
@@ -11,12 +12,13 @@ interface MediaItemProps {
 }
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
     const imageUrl = useLoadImage(data)
+    const player = usePlayer()
     const handleClick = () => {
         if (onClick){
             return onClick(data.id)
         }
 
-        // TODO: Default turn on player
+        return player.setId(data.id)
     }
     
     return (
